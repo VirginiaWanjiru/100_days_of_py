0197170ddbc2df2b6043dcc6c3c03623688a13bd
Virginia Wanjiru <125539295+furaha-in-ctrl@users.noreply.github.com>
2023-09-30 20:30:42 +0300

    user input

---
+++ user_inputs.py
@@ -0,0 +1,20 @@
+myName=input("Whats your name ?")
+print("Hey" + myName)
+
+myMeal=input("What is your fav meal?")
+print("Very nice ")
+print(myMeal)
+
+myMusic=input("What is your fav music?")
+print("Cool taste ")
+print(myMusic)
+
+NapoIshi=input("Where do you live?")
+print("Ulala")
+print(NapoIshi)
+
+print("I would like to visit you someday ",myName ," in",NapoIshi,"and we could have some", myMeal,"while listening to",myMusic,".")
+
+
+
+
