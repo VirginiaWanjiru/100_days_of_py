1f8b2886b3929ac5fbb46e65c5b202ba032c3eb2
Virginia Wanjiru <125539295+furaha-in-ctrl@users.noreply.github.com>
2023-09-30 20:16:32 +0300

    first commit

---
+++ concatenation.py
@@ -0,0 +1,17 @@
+# Concatenation -Day 3 
+varName ="Conjestina"
+print(varName, "is great")
+print("She fought ",varName)
+
+myLunch= input("What will I have for lunch?")
+print("Maybe I will have ", myLunch, "for lunch.")
+
+###coding challenge 
+food="Biriyani"
+plant="Palm"
+method="fry"
+foodDescription="soggy"
+DiyItem="Pliers"
+
+print("MENU")
+print(method, food,"with", foodDescription,plant,"on a bed of",DiyItem)
