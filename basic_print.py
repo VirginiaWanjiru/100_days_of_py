8033da4bfdb969623c2cbeff31dc401d1a79d801
Virginia Wanjiru <125539295+furaha-in-ctrl@users.noreply.github.com>
2023-09-30 20:22:21 +0300

    first commit

---
+++ basic_print.py
@@ -0,0 +1,21 @@
+
+#print as it is - use of (""" """) 
+print("""
+Virginia 
+September 26th 2023
+
+I am excited to sign up for the Replits 100 #
+days of code of Python Challenge!
+
+I will make sure to spend time everyday coding along for a minimum of 10 min a day. 
+
+I will be using Replit an amazing  online IDE so I can do this from my devices wherever I happen to be. Lets go!!!
+
+Follow my progress at replit.com/virgie_100daysofcode
+#eagertolearn
+#replits100DaysofCode
+
+
+
+
+""")
