fcb8b8e674ab878ea909e08f2d177edd4674fd5c
Virginia Wanjiru <125539295+furaha-in-ctrl@users.noreply.github.com>
2023-10-04 18:42:57 +0300

    if else statements

---
+++ if_else.py
@@ -0,0 +1,20 @@
+print("Marvel Character Creator")
+
+spiderman=input("Do you like hanging around?")
+if spiderman == "yes":
+   print("You are spiderman")
+else:
+   print("Then you are not spiderman :(")
+
+captainAmerica=input("Do you carry a shield with the american flag?")
+if captainAmerica == "yes":
+  print("Horaay its Sssuperman")
+else :
+  print("You are not captain america :(")
+
+wonderwoman=input("Are you the Amazon Princess and the most powerful warrior of your race?")
+if wonderwoman == "yes":
+  print("Glad to meet you Wonderwoman.You are a wonder")
+else:
+  print("Your not wonderwoman :(")
+   
